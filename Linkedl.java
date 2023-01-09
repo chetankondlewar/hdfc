5caaba9a7d1f0945218ac7f088339d97666b408e
chetankondlewar <chetan.kondlewar@nciportal.com>
2023-01-09 14:20:16 +0530

    gh

--- Linkedl.java
+++ Linkedl.java
@@ -15,27 +15,8 @@ public class Linkedl {
 		list.add(50);
 		System.out.println(list);
 		
-		LinkedList<Integer> list1=new LinkedList<>(list);
-		list1.add(60);
-		list1.add(40);
-		list1.add(30);
-		list1.add(20);
-		list1.add(50);
-		System.out.println(list1);
 		
-		list1.add(2, 100);
-		System.out.println(list1);
-		System.out.println(list1.contains(100));
-		System.out.println(list1.get(2));
-		System.out.println(list1.indexOf(100));
-		System.out.println(list1.lastIndexOf(50));
-		list1.remove(2);
-		System.out.println(list1);
-		System.out.println(list1.size());
-		list1.set(2, 100);
-		System.out.println(list1);
-		list1.addFirst(200);
-		System.out.println(list1);
+		
 		
 	}
 
